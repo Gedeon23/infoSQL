e6cd002f047c1d6baa4a49cd627495fa14106ed5
Gedeon <gedeon.jahns@web.de>
2020-12-01 05:15:09 +0100

    revert

--- socialmedia/posts/views.py
+++ socialmedia/posts/views.py
@@ -45,7 +45,7 @@ def edit_post_view(request, id):
             form = Post_Creation_Form(instance=post)
             return render(request, 'post/edit.html', {'form': form, 'request': request})
     else:
-        return redirect('/error/you can only edit your own posts sike/')
+        return redirect('/error/you can only edit your own posts /')
 
 
 def delete_post(request, id):
