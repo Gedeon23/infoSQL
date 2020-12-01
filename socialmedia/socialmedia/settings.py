ed0333c29ed63ecea04e86238cd79b1ee020bad3
Gedeon <gedeon.jahns@web.de>
2020-12-01 02:30:18 +0100

    DB settings

--- socialmedia/socialmedia/settings.py
+++ socialmedia/socialmedia/settings.py
@@ -84,12 +84,24 @@ WSGI_APPLICATION = 'socialmedia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'DB',
+            'USER': 'DB_admin',
+            'PASSWORD': 'Kasimir2015',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
