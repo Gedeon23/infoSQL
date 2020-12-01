0e203c636b5c20a6b0561d7ad3ba68a46542af21
Gedeon <gedeon.jahns@web.de>
2020-12-01 02:49:11 +0100

    static

--- socialmedia/socialmedia/settings.py
+++ socialmedia/socialmedia/settings.py
@@ -144,6 +144,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
